e9f16261672393f7b554fb51996ac9ce1e0a75a1
Tiffany ANTOINE <tiffany.antoine38@gmail.com>
2023-02-25 17:14:17 +0100

    update

---
+++ C04/ft_atoi.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+int ft_atoi(char *str)
+{
+	int i;
+	int sign;
+	int result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while(str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	while(str[i] == '+' || str[i] == '_')
+	{
+		if(str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while(str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return(result * sign);
+}
