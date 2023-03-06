85a8e0dead0baee4e7f6205d2f212fba1673ea49
katicawa <tiffany.antoine38@gmail.com>
2023-03-06 20:45:24 +0100

    error fixed

--- C04/ft_putnbr_base.c
+++ C04/ft_putnbr_base.c
@@ -17,37 +17,41 @@ int    ft_base_len(char *base)
 
 int ft_check_base(char *base)
 {
-	int				i;
-	unsigned char	check[256]; //taille de la table ascii
+    int            i;
+//taille de la table ascii
+    unsigned char  check[256];
 
     i = 0;
     while (i < 256)
+//nb de fois qu'on tombre sur un char
         check[i++] = 0;
     i = 0;
     while (base[i])
     {
-        check[(unsigned char)base[i]]++; //check le nb de fois qu'on tombe sur un char
+//vérifie le nb de fois qu'on est tombé sur un char
+        check[(unsigned char)base[i]]++;
         if (check[(unsigned char)base[i]] > 1)
             return (1);
         i++;
     }
+//vérifie si la chaine contient des "+" ou des "-"
     if (check['+'] || check['-'])
         return (1);
-	return (0);
+     return (0);
 }
 
 void    ft_putnbr_base(int nbr, char *base)
 {
     int  base_len;
-	int  denominator;
+    int  denominator;
     int  result;
 
     base_len = ft_base_len(base);
     denominator = 1;
-	if (ft_check_base(base) == 1)
-		return;
-	if (ft_base_len(base) < 2)
-		return;
+    if (ft_check_base(base) == 1)
+        return;
+    if (base_len < 2)
+        return;
 //gére les négatifs
     if (nbr < 0)
     {
